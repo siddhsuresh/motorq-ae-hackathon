888f7dc61a33956a821fe2d8a4855c0a90f65839
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-08-19 14:21:23 +0530

    Update updateEvent.ts

--- app/events/mutations/updateEvent.ts
+++ app/events/mutations/updateEvent.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { z } from "zod";
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { z } from "zod"
 
 const UpdateEvent = z.object({
   id: z.string(),
@@ -12,20 +12,20 @@ const UpdateEvent = z.object({
   latitude: z.number(),
   logitude: z.number(),
   size: z.number(),
-});
+})
 
 export default resolver.pipe(
   resolver.zod(UpdateEvent),
   resolver.authorize("ADMIN"),
-  async ({ id, timeStart, timeEnd,...data }) => {
+  async ({ id, timeStart, timeEnd, ...data }) => {
     const timeStart_d = new Date(timeStart)
     const timeEnd_d = new Date(timeEnd)
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const event = await db.event.update({ where: { id }, 
-                                         timeStart: timeStart_d,
-                                         timeEnd: timeEnd_d,
-                                         data });
+    const event = await db.event.update({
+      where: { id },
+      data: { ...data, timeStart: timeStart_d, timeEnd: timeEnd_d },
+    })
 
-    return event;
+    return event
   }
-);
+)
